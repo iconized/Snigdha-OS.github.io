5039a9968e756b59b4c3410e69df642a25cdb160
Abhiraj Roy <157954129+iconized@users.noreply.github.com>
2024-06-10 03:49:12 +0530

    chore(re): delete *

--- components/common/Section.js
+++
@@ -1,9 +0,0 @@
-export default function Section({ title, children, className, ...props }){
-    return (
-        <section {...props} className={`${className} py-8 md:py-12`}>
-            <h2>{ title }</h2>
-            <div className="hr"></div>
-            { children }
-        </section>
-    )
-}
